1019854a3fa0ef8fb8f30c1fd9bb3d60507c7046
Kerem Deen <Kerem.deen@gmail.com>
2023-08-08 14:01:25 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(array) {
+    const newArray = array ;
+    for (let count = 0;count < array.length;count++) {
+        newArray[count] = `Thank you, ${array[count]}, for the wonderful surprise gift!`;
+    }
+    return newArray;
+}
+
+writeCards(["Bob", "Simp", "Cougar"])
+
+function countDown(n) {
+    for (let i = n; i >= 0; i--) {
+        console.log(i);
+    }
+
+}
